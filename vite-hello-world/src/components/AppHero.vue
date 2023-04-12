566d9e9c0d255bc7eb6a473b1527623f91432a7e
NelloRossi <rossianiello97@gmail.com>
2023-04-12 13:58:15 +0200

    added hero components to the app

---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h2>{{ text }}</h2>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero',
+        data(){
+            return{
+                text: 'prova'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
