ea5fd4a387cdaef1dd0bef8d294287834fade9fc
NelloRossi <rossianiello97@gmail.com>
2023-04-12 13:50:25 +0200

    added title component to the app

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data(){
+            return{
+                title: 'Hello World from Vite!'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
